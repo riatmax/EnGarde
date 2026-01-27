771224bcf5e6905423868e1685e54c8810920385
riatmax <riatmax@gmail.com>
2026-01-27 10:38:51 -0500

    removed code and started on movement

--- Assets/Scripts/PlayerAvatar.cs
+++ Assets/Scripts/PlayerAvatar.cs
@@ -4,87 +4,36 @@ using UnityEngine;
 
 public class PlayerAvatar : MonoBehaviour
 {
-    public GameObject oppAv;
+    [Header("Movement Info")]
+    [SerializeField] private float moveSpeed;
+    private PlayerInput inputActions;
+    private Vector2 moveInput;
+
+
     public Animator ani;
     public float distChars;
     public bool closing = true;
-    public Vector2 MoveRange;
-    public float MoveTarget;
-    void Start()
+
+    private void Awake()
     {
-        //Advance();
-        SetTarget();
+        inputActions = new PlayerInput();
     }
 
-    void Update()
+    private void OnEnable()
     {
-        if (Vector3.Distance(transform.position, new Vector3(MoveTarget, transform.position.y, 0)) < .01f)
-        {
-            SetTarget();
-        }
-        /*distChars = oppAv.transform.position.x - transform.position.x;
-        if (transform.position.x <= -4.6)
-        {
-            closing = false;
-        }
-        if (transform.position.x >= 0)
-        {
-            closing = true;
-        }*/
+        inputActions.Enable();
     }
 
-    void Advance()
+    private void OnDisable()
     {
-        ani.Play("PlayerAdvance");
-        transform.position += new Vector3(.5f, 0, 0);
+        inputActions.Disable();
     }
-    void Lunge()
+
+    private void Update()
     {
-        ani.Play("PlayerLunge");
-        transform.position += new Vector3(1, 0, 0);
-    }
-    void Retreat()
-    {
-        ani.Play("PlayerRetreat");
-        transform.position -= new Vector3(.5f, 0, 0);
-    }
-    void Parry()
-    {
-        ani.Play("PlayerParry");
-    }
-    public void CloseDist()
-    {
-        if (MoveTarget < transform.position.x)
-        {
-            Retreat();
-        }
-        else if (MoveTarget > transform.position.x)
-        {
-            Advance();
-        }
-        /*if (closing)
-        {
-            if (distChars > 1)
-            {
-                Advance();
-            }
-            else if (distChars < 1)
-            {
-                Retreat();
-            }
-        }
-        else
-        {
-            Advance();
-        }*/
-    }
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawLine(new Vector3(MoveRange.x, transform.position.y, 0), new Vector3(MoveRange.y, transform.position.y, 0));
-    }
-    private void SetTarget()
-    {
-        MoveTarget = Random.Range(MoveRange.x, MoveRange.y);
+        moveInput = inputActions.Player.Move.ReadValue<Vector2>();
+
+        Vector3 movement = new Vector3(moveInput.x, moveInput.y, 0);
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
     }
 }
